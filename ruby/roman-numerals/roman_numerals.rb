c67bdae6eee9b16fd1347cbbf791e6b01e810ffb
Eric Yulianto <eric.yulianto.ang@gmail.com>
2016-10-19 23:28:18 +0800

    Refactor ruby roman-numerals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -3,20 +3,26 @@ module BookKeeping
 end
 
 class Fixnum
+  NUMBER_TO_ROMAN_MAP = {
+    1000 => "M",
+    900  => "CM",
+    500  => "D",
+    400  => "CD",
+    100  => "C",
+    90   => "XC",
+    50   => "L",
+    40   => "XL",
+    10   => "X",
+    9    => "IX",
+    5    => "V",
+    4    => "IV",
+    1    => "I",
+  }
+
   def to_roman
     return "" unless self > 0 and self < 4000
-    return "M" + (self - 1000).to_roman if self >= 1000
-    return "CM" + (self - 900).to_roman if self >= 900
-    return "D" + (self - 500).to_roman if self >= 500
-    return "CD" + (self - 400).to_roman if self >= 400
-    return "C" + (self - 100).to_roman if self >= 100
-    return "XC" + (self - 90).to_roman if self >= 90
-    return "L" + (self - 50).to_roman if self >= 50
-    return "XL" + (self - 40).to_roman if self >= 40
-    return "X" + (self - 10).to_roman if self >= 10
-    return "IX" + (self - 9).to_roman if self >= 9
-    return "V" + (self - 5).to_roman if self >= 5
-    return "IV" + (self - 4).to_roman if self >= 4
-    return "I" + (self - 1).to_roman if self >= 1
+    NUMBER_TO_ROMAN_MAP.each_pair do |number, roman|
+      return roman + (self - number).to_roman if self >= number
+    end
   end
 end
