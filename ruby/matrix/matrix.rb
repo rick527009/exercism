af31104f0ba50a838bf730121c06f90f20bdfe6d
Eric Yulianto <eric.yulianto.ang@gmail.com>
2019-05-08 23:19:19 +0800

    refactor ruby matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,40 +1,18 @@
 class Matrix
-  class ParseError < RuntimeError; end
-
   attr_reader :rows, :columns
 
-  def initialize(values)
-    @values = values
-    parse_values!
+  def initialize(matrix_string)
+    @rows = parse_matrix(matrix_string)
+    @columns = @rows.transpose
   end
 
 private
 
-  def parse_values!
-    @rows = @values.split("\n").map { |row| parse_row(row) }
-    raise ParseError.new("Unequal row length") unless rows_equal_length?
-    calculate_columns
+  def parse_matrix(matrix_string)
+    matrix_string.each_line.map { |row| parse_row(row) }
   end
 
   def parse_row(row)
-    row.split.map { |value| value.to_i }
-  end
-
-  def rows_equal_length?
-    @rows.all? { |row| row.count == first_row_count }
-  end
-
-  def calculate_columns
-    @columns = []
-    first_row_count.times { @columns << [] }
-    @rows.each do |row|
-      row.each.with_index do |value, i|
-        @columns[i] << value
-      end
-    end
-  end
-
-  def first_row_count
-    @first_row_count ||= @rows.first.count
+    row.split.map(&:to_i)
   end
 end
