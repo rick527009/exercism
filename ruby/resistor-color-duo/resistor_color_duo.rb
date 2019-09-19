5da3f9eb6723bd0c7a70e77f1411711bb8147594
Eric Yulianto <eric.yulianto.ang@gmail.com>
2019-09-19 23:31:15 +0800

    refactor ruby resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,18 +1,7 @@
 class ResistorColorDuo
-  COLOR_VALUE = {
-    "black"  => 0,
-    "brown"  => 1,
-    "red"    => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green"  => 5,
-    "blue"   => 6,
-    "violet" => 7,
-    "grey"   => 8,
-    "white"  => 9
-  }
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(colors)
-    COLOR_VALUE[colors[0]] * 10 + COLOR_VALUE[colors[1]]
+    COLORS.index(colors[0]) * 10 + COLORS.index(colors[1])
   end
 end
